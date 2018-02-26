f89cea4d942c72ba13fee097b7084c2dad7dc3ad
anatanson <alexa.natanson@gmail.com>
2018-02-25 18:04:55 -0800

    added css

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
 
-
-//for trying soundcloud :) -->
 window.onload = function() {
 SC.initialize({
   client_id: 'b67ae3e0a89f0afc33278a1f858d8525'
 });
 };
-// SC.stream("/tracks/293", function(sound){
-//     $("audio-test").attr("src", sound.uri);
-// });
-
-// var getTracks = function () {
-//   return SC.get('/me/tracks', { limit: 1 });
-// };
-//
-// SC.get('/tracks', {
-//   q: 'buskers', license: 'cc-by-sa'
-// }).then(function(tracks) {
-//   console.log(tracks);
-// });
-//
-// //stream track id 293
-// //find all sounds of buskers licensed under 'creative commons share alike'
-// SC.get('/tracks', {
-//   q: 'buskers',
-// }).then(function(tracks) {
-//   console.log(tracks);
-
-  // SC.stream(tracks(0)).then(function(player){
-  //   player.play().then(function(){
-  //     console.log('Playback started!');
-  //   }).catch(function(e){
-  //     console.error('Playback rejected. Try calling play() from a user interaction.', e);
-  //   });
-  // });
-// });
-
-
 
 
 var $searchInput = $('.search');
@@ -67,27 +34,36 @@ function makeGif(src = '//giphy.com/embed/') {
   return `<img src="${src}" />`;
 }
 
+
+
 $button.on('click', e => {
   search.text = $searchInput.val();
+
+
+  $.get(
+    'https://api.soundcloud.com/tracks?&tags=' + $searchInput.val() + '&client_id=b67ae3e0a89f0afc33278a1f858d8525',
+    function (result) {
+      var track = result[0];
+      console.log(track);
+      SC.stream("tracks/" + track.id).then(function(player){
+        player.play()
+      });
+    }
+  );
+
+
+
   search.fetch(url => {
       $resultWrapper.html(makeGif(url));
 });
 
 
-
-$.get(
-  'https://api.soundcloud.com/tracks?&tags=' + $searchInput.val() + '&client_id=b67ae3e0a89f0afc33278a1f858d8525',
-  function (result) {
-  //  console.log(result[0]);
-    var track = result[0];
-    console.log(track);
-    SC.stream("tracks/" + track.id).then(function(player){
-      player.play()
-    });
-  }
-);
-
-
-
-
 }); //closes button click
+
+
+//replicate function if user presses 'enter', doesn't click button
+$('#search').keypress(function(e){
+        if(e.which == 13){
+            $('#submit').click();
+        }
+    });
